172b49ec5c5e99fab4775196a9d4ab060f86fe17
Murillo Ivamoto <ivamoto.murillo@gmail.com>
2017-11-28 21:07:54 -0200

    Initial projects

---
+++ app1/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class App extends Component<{}> {
+    render() {
+        return (
+            <Text>Meu primeiro app</Text>
+        );
+    }
+}
